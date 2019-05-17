36d74c80b5e53a7825dbf62bee2bc5b6f9a981fd
Carlos Flores <sudocarlosflores@protonmail.com>
2019-05-17 09:10:28 -0700

    Create start-vim.sh

---
+++ start-vim.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo 'Installing pathogen.vim'
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo 'Cloning nerdcommenter'
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+
+# For persistent undo
+mkdir ~/.vim/undodir
+
+curl 'https://raw.githubusercontent.com/carlosflrs/dotfiles/master/vimrc' > ~/.vimrc
